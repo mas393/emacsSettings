164900361ba7aed343ebf79114e6b168f4ca4743
matthew <matthewstephenson39@gmail.com>
2021-01-12 20:41:01 -0700

    inital commit

---
+++ init.el
@@ -0,0 +1,14 @@
+(require 'package)
+;;(setq package-enable-at-startup nil)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+(require 'org-tempo)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+
+;;;CUSTOM SET VARIABLES MUST STAY
