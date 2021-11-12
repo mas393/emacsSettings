1555057a94d7fbfbd046c58a18012c76b5b8e301
matthew <matthewstephenson39@gmail.com>
2021-11-11 20:33:52 -0800

    updated nov 2021

--- init.el
+++ init.el
@@ -1,5 +1,5 @@
 (require 'package)
-;;(setq package-enable-at-startup nil)
+(setq package-enable-at-startup nil)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
@@ -9,6 +9,26 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
+
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
 
+
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((emacs-lisp . t)
+   (lisp . t)))
+
 ;;;CUSTOM SET VARIABLES MUST STAY
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(markdown-mode magit-badge magit multiple-cursors dmenu slime popup-kill-ring which-key use-package ido-vertical-mode avy)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
