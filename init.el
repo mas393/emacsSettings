7afd7d04076afb63c5ae2515a4735024fb0bf655
matthew <matthewstephenson39@gmail.com>
2022-03-22 05:49:28 -0600

    2022-03-22

--- init.el
+++ init.el
@@ -3,7 +3,8 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
-(require 'org-tempo)
+;;(require 'org-tempo)
+(require 'org)
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
@@ -13,10 +14,34 @@
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
 
 
+;; Testing
+(setq scroll-conservatively 10000)
+(use-package good-scroll
+  :ensure t
+  :config
+  (good-scroll-mode 1)
+  )
+
 (org-babel-do-load-languages
  'org-babel-load-languages
- '((emacs-lisp . t)
-   (lisp . t)))
+ '(
+   (C . t)
+   (python . t)
+   (emacs-lisp . t)))
+
+(setq org-babel-python-command "python3"
+      org-src-fontify-natively t
+      org-src-preserve-indentation t
+      org-src-tabs-acts-natively t
+      org-confirm-babel-evaluate nil
+      org-adapt-indentation nil)
+
+(global-set-key (kbd "C-=") 'text-scale-increase)
+(global-set-key (kbd "C--") 'text-scale-decrease)
+
+
+
+
 
 ;;;CUSTOM SET VARIABLES MUST STAY
 (custom-set-variables
@@ -24,11 +49,21 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
+ '(custom-safe-themes
+   '("fe845acbd6169cb2d1f3f3bf0e761cbee512359438da2ec170aa49400631f853" "77a70a104f0eefd1bade76d53ba6cadfbb6c87eeb9f358acd85d6c5ce9aa02d6" "5f4dfda04fbf7fd55228266c8aab73953d3087cea7fd06dd7f8ff1e4a497c739" default))
  '(package-selected-packages
-   '(markdown-mode magit-badge magit multiple-cursors dmenu slime popup-kill-ring which-key use-package ido-vertical-mode avy)))
+   '(org-bullets good-scroll rainbow-delimiters flycheck company origami org-journal markdown-mode magit-badge magit multiple-cursors dmenu slime popup-kill-ring which-key use-package ido-vertical-mode avy)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(rainbow-delimiters-base-face ((t (:inherit nil))))
+ '(rainbow-delimiters-depth-1-face ((t (:inherit rainbow-delimiters-base-face :foreground "white"))))
+ '(rainbow-delimiters-depth-2-face ((t (:inherit rainbow-delimiters-base-face :foreground "deep sky blue"))))
+ '(rainbow-delimiters-depth-3-face ((t (:inherit rainbow-delimiters-base-face :foreground "dark orange"))))
+ '(rainbow-delimiters-depth-4-face ((t (:inherit rainbow-delimiters-base-face :foreground "dark khaki")))))
